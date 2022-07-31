1464b2c1546eb784c69342bb16eff1c2ee31b705
Gebreyowhans <gebreyohans2005@gmail.com>
2022-07-31 23:11:33 +0200

    created the first DB migration

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,11 +1,22 @@
 import http
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from listings.models import Band
 # Create your views here.
 
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django ! </>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+    <h1>Hello Django !</h1>'
+    <p>My favorite bands are:<p>
+    <ul>
+     <li>{bands[0].name}</li>
+     <li>{bands[1].name}</li>
+     <li>{bands[2].name}</li>
+    </ul>
+    """)
 
 
 def about(request):
