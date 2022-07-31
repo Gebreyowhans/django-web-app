8c7c582bbc6ce44831ac87b88907747f9ce722fb
Gebreyowhans <gebreyohans2005@gmail.com>
2022-07-31 14:03:50 +0200

    some additional pages added

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,4 @@
+import http
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
@@ -5,3 +6,15 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse('<h1>Hello Django ! </>')
+
+
+def about(request):
+    return HttpResponse('<h1>About us </h1> <p>we love merch !</p>')
+
+
+def listings(request):
+    return HttpResponse('<h1>This is my listing page </h1> <p>later on we will show you the list of service we offer in this website !</p>')
+
+
+def contacts(request):
+    return HttpResponse('<h1>This is the contact page </h1> <p> her you can find our contact details !</p>')
